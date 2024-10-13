6195d39c0050161e212baf09136250b2de56387e
Samir Warsi <warsisamir78.com>
2024-10-13 11:50:43 +0530

    adding fe link

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://e100xdevs.netlify.app",
     credentials: true,
 }))
 
