1ac5b814560e5f7a693d0ff0a2a29a532474b67e
Samir Warsi <warsisamir78.com>
2024-11-20 17:41:09 +0530

    changing link

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,13 @@ const { adminRouter } = require("./router/admin")
 const { userRouter } = require("./router/user")
 const { courseRouter } = require("./router/course")
 const cookieParser = require("cookie-parser")
-
+const path = require("path")
 const bodyParser = require("body-parser")
 // const { required } = require('joi')
 const PORT = process.env.PORT || 5000
 
 
-
+app.use(express.static(path.join(__dirname, 'client','dist')))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cookieParser())
@@ -27,7 +27,9 @@ app.use(cors({
 // app.use("/api/admin", adminRouter)
 app.use("/api/user", userRouter)
 app.use("/api/course", courseRouter)
-
+app.get("*",(req, res) =>{
+    res.sendFile(path.resolve(__dirname,'client','dist','index.html'))
+})
 
 const main =async()=>{
     try{
@@ -35,8 +37,6 @@ const main =async()=>{
         console.log("Data base connected successfully")
         app.listen(PORT)
         console.log(`Server running on port  ${PORT}`)
-        
-
     }catch(error){
         console.error(error)
        
